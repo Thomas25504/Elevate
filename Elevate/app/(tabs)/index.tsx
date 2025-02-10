aecfffc33ea4072f0684fcdbee826abffe952dbb
Tom Davidson <tldavidson2004@gmail.com>
2025-02-10 21:38:34 +0000

    Refactor HomeScreen layout to include additional sections for Progress, Workouts, and Macros with improved styling

--- Elevate/app/(tabs)/index.tsx
+++ Elevate/app/(tabs)/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View
       style={{
         flex: 1,
+        flexDirection: 'column',
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Home Screen</Text>
+      <View
+      style={styles.container}
+    >
+      <Text>Progress</Text>
     </View>
+    <View
+      style={styles.container}
+    >
+      <Text>Workouts</Text>
+    </View>
+    <View
+      style={styles.container}
+    >
+      <Text>Macros</Text>
+    </View>
+    
+    </View>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '90%',
+    margin: 10,
+    padding: 15,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 30,
+  }
+});
